6532c53933500ed017b4dc1e67982add32c71857
K4rian <contact@k4rian.com>
2025-02-10 08:23:03 +0000

    Fix field formatting logic in console output

--- formatters.go
+++ formatters.go
@@ -2,19 +2,20 @@ package dslogger
 
 import "fmt"
 
-func formatConsoleFields(separator string, fields ...interface{}) string {
+func formatConsoleFields(fieldSeparator string, consoleSeparator string, fields ...interface{}) string {
 	if len(fields) == 0 {
 		return ""
 	}
 
 	formatted := ""
-	for i := 0; i < len(fields); i += 2 {
-		if i+1 < len(fields) {
-			formatted += fmt.Sprintf("%v%s%v ", fields[i], separator, fields[i+1])
+	fieldsLen := len(fields)
+	for i := 0; i < fieldsLen; i += 2 {
+		if i+1 < fieldsLen {
+			formatted += fmt.Sprintf("%v%s%v", fields[i], fieldSeparator, fields[i+1])
+			if i+2 < fieldsLen {
+				formatted += consoleSeparator
+			}
 		}
-	}
-	if len(formatted) > 0 {
-		formatted = formatted[:len(formatted)-1]
 	}
 	return formatted
 }
@@ -29,7 +30,7 @@ func (l *Logger) formatMessage(msg string, fields ...interface{}) string {
 	}
 
 	if len(fields) > 0 {
-		msg += l.config.ConsoleSeparator + formatConsoleFields(l.config.FieldSeparator, fields...)
+		msg += l.config.ConsoleSeparator + formatConsoleFields(l.config.FieldSeparator, l.config.ConsoleSeparator, fields...)
 	}
 	return msg
 }
